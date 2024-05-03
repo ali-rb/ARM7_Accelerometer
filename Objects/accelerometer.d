ef688030a0669b970d85f27fb65540828fbc433e
Ali Ranjbar <a.randjbarr@gmail.com>
2024-05-03 14:09:47 +0330

    Add files via upload

---
+++ Objects/accelerometer.d
@@ -0,0 +1,4 @@
+.\objects\accelerometer.o: Accelerometer.c
+.\objects\accelerometer.o: C:\Keil_v5\ARM\Inc\Philips\lpc214x.h
+.\objects\accelerometer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\accelerometer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
