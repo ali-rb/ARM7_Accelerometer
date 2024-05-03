8a06f52e49c9ebcb51a01b5e98189fde7317717c
Ali Ranjbar <a.randjbarr@gmail.com>
2024-05-03 14:07:45 +0330

    Delete Objects directory

--- Objects/accelerometer.d
+++
@@ -1,4 +0,0 @@
-.\objects\accelerometer.o: Accelerometer.c
-.\objects\accelerometer.o: C:\Keil_v5\ARM\Inc\Philips\lpc214x.h
-.\objects\accelerometer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\accelerometer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
